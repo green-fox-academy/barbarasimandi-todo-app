5eee5eeb44c3d4ddbe9ca367b06a25c3f2632fea
unknown <barbarasimandi@gmail.com>
2017-10-20 09:33:49 +0200

    removing .class

--- test/InspectorTest.java
+++ test/InspectorTest.java
@@ -1,5 +1,17 @@
 import static org.junit.Assert.*;
 
+import org.junit.Test;
+
 public class InspectorTest {
+
+  String testInspector;
+
+  @Before
+  public void setUp() throws Exception {
+    testInspector = "";
+  }
+
+  @Test
+  public void
 
 }
